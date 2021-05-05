d43b977a9862f9f72d0c1f0be2a238032211ac3e
Neek <Sp-Neek@yandex.ru>
2021-05-06 00:26:08 +0700

    Работа с физикой Аdd Force
    Создаю игру на физике

---
+++ LearnPhisics/Assets/physicalforce.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class physicalforce : MonoBehaviour
+{
+    private Rigidbody R;
+    // Start is called before the first frame update
+    void Start()
+    {
+        R = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            R.AddForce(0,  900, 0);
+        }   
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            R.AddForce(-300,0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            R.AddForce(300, 0, 0);
+        }
+    }
+}
---
+++ LearnPhisics/Assets/wall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    Rigidbody R;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        R = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        R.velocity = new Vector3(0, 0, -speed);
+    }
+}
